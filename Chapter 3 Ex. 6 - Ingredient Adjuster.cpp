2671798b016635926e5414cecd8de4d9325731e8
sterl <sterlingrodgers2007@gmail.com>
2025-09-28 15:43:27 -0400

    comments

--- Chapter 3 Ex. 6 - Ingredient Adjuster.cpp
+++ Chapter 3 Ex. 6 - Ingredient Adjuster.cpp
@@ -10,13 +10,17 @@ using namespace std;
 
 int main()
 {
+	//Create necessary variables
 	double sugar = 1.5, butter = 1, flour = 2.75, cookie = 48, cookiea, cookien;
 
+	//Ask the user how many cookies they want to make and save this value
 	cout << "How many cookies do you want to make? ";
 	cin >> cookiea;
 
+	//Equation used to calculate how many cookies will be needed which will be used to calculate cups of each ingredient  
 	cookien = cookiea / cookie;
 
+	//Calculate and display the necessary number of cups needed to make however many cookies
 	cout << "You will need the following ingredients in order to make " << cookiea << " cookies: \n";
 	cout << sugar * cookien << " cups of sugar\n";
 	cout << butter * cookien << " cups of butter\n";
